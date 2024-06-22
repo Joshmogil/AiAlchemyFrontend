47becadf64885b7724a1e9ee34ab6b8657837c85
Josh Mogil <jmogil@pop-os.localdomain>
2024-06-22 13:20:59 -0400

    adding htmx

--- alchemy-api/app/db.py
+++ alchemy-api/app/db.py
@@ -3,6 +3,9 @@ from typing import AsyncGenerator
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import select
 from sqlalchemy.orm import DeclarativeBase
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
@@ -14,6 +17,13 @@ class Base(DeclarativeBase):
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
     pass
+
+class ToDo(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    content = Column(String)
+    session_key = Column(String)
 
 
 engine = create_async_engine(DATABASE_URL)
@@ -32,3 +42,39 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
     yield SQLAlchemyUserDatabase(session, User)
+
+async def create_todo(content: str, session_key: str, session: AsyncSession = Depends(get_async_session)):
+    todo = ToDo(content=content, session_key=session_key)
+    session.add(todo)
+    await session.commit()
+    await session.refresh(todo)
+    return todo
+
+
+async def get_todo(item_id: int, session: AsyncSession = Depends(get_async_session)) -> ToDo | None:
+    stmt = select(ToDo).where(ToDo.id == item_id)
+    result = await session.execute(stmt)
+    return result.scalars().first()
+
+
+async def update_todo(item_id: int, content: str, session: AsyncSession = Depends(get_async_session)):
+    todo = await get_todo(item_id, session)
+    if todo is None:
+        return None
+    todo.content = content # type: ignore
+    await session.commit()
+    await session.refresh(todo)
+    return todo
+
+
+async def get_todos(session_key: str, skip: int = 0, limit: int = 100, session: AsyncSession = Depends(get_async_session)):
+    stmt = select(ToDo).where(ToDo.session_key == session_key).offset(skip).limit(limit)
+    result = await session.execute(stmt)
+    return result.scalars().all()
+
+
+async def delete_todo(item_id: int, session: AsyncSession = Depends(get_async_session)):
+    todo = get_todo(item_id, session)
+    await session.delete(todo)
+    await session.commit()
+    return todo
--- alchemy-api/app/main.py
+++ alchemy-api/app/main.py
@@ -3,23 +3,20 @@ import uuid
 from contextlib import asynccontextmanager
 
 from fastapi import Depends, FastAPI, Form, Request, Response
-
-
 from fastapi import FastAPI
-
-
 from fastapi.responses import HTMLResponse
-
 from fastapi.templating import Jinja2Templates
-from sqlalchemy.orm import Session
 
-from app.database import Base, SessionLocal, engine
+from sqlalchemy.ext.asyncio import AsyncSession
 
+import fastapi_users as fp
+
+from app.db import get_async_session
 from app.db import User, create_db_and_tables
+from app.db import create_todo, get_todo, update_todo, get_todos, delete_todo
+
 from app.schemas import UserCreate, UserRead, UserUpdate
 from app.users import auth_backend, current_active_user, fastapi_users
-
-from app.models import create_todo ,delete_todo ,get_todo ,get_todos ,update_todo
 
 
 @asynccontextmanager
@@ -57,27 +54,37 @@ app.include_router(
     tags=["users"],
 )
 
-
 @app.get("/authenticated-route")
 async def authenticated_route(user: User = Depends(current_active_user)):
     return {"message": f"Hello {user.email}!"}
 
+@app.post("/add", response_class=HTMLResponse)
+async def post_add(request: Request, content: str = Form(...), session: AsyncSession = Depends(get_async_session)):
+    session_key = request.cookies.get("session_key")
+    todo = await create_todo(content=content, session_key=session_key, session=session)
+    context = {"request": request, "todo": todo}
+    return templates.TemplateResponse("todo/item.html", context)
 
+@app.get("/edit/{item_id}", response_class=HTMLResponse)
+async def get_edit(request: Request, item_id: int, session: AsyncSession = Depends(get_async_session)):
+    todo = await get_todo(item_id= item_id, session= session)
+    context = {"request": request, "todo": todo}
+    return templates.TemplateResponse("todo/form.html", context)
 
+@app.put("/edit/{item_id}", response_class=HTMLResponse)
+async def put_edit(request: Request, item_id: int, content: str = Form(...), session: AsyncSession = Depends(get_async_session)):
+    todo = await update_todo(item_id, content, session)
+    context = {"request": request, "todo": todo}
+    return templates.TemplateResponse("todo/item.html", context)
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+@app.delete("/delete/{item_id}", response_class=Response)
+async def delete(item_id: int, session: AsyncSession = Depends(get_async_session)):
+    await delete_todo(item_id, session)
 
-
-
-@app.get("/", response_class=HTMLResponse)
-def home(request: Request, db: Session = Depends(get_db)):
+@app.get("/user-home", response_class=HTMLResponse)
+async def home(request: Request, session: AsyncSession = Depends(get_async_session)):
     session_key = request.cookies.get("session_key", uuid.uuid4().hex)
-    todos = get_todos(db, session_key)
+    todos = await get_todos(session_key=session_key, session=session)
     context = {
         "request": request,
         "todos": todos,
@@ -87,35 +94,29 @@ def home(request: Request, db: Session = Depends(get_db)):
     response.set_cookie(key="session_key", value=session_key, expires=259200)  # 3 days
     return response
 
+@app.get("/authenticated-route")
+@app.get("/crucible", response_class=HTMLResponse)
+async def crucible(request: Request, session: AsyncSession = Depends(get_async_session), user: User = Depends(current_active_user)):
+    session_key = request.cookies.get("session_key", uuid.uuid4().hex)
+    todos = await get_todos(session_key=session_key, session=session)
+    context = {
+        "request": request,
+        "todos": todos,
+        "title": "Home"
+    }
+    response = templates.TemplateResponse("home.html", context)
+    response.set_cookie(key="session_key", value=session_key, expires=259200)  # 3 days
+    return response
 
+@app.get("/main", response_class=HTMLResponse)
+async def main_page(request: Request):
+    return templates.TemplateResponse("main.html", {"request": request})
 
+@app.get("/login", response_class=HTMLResponse)
+async def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
 
-@app.post("/add", response_class=HTMLResponse)
-def post_add(request: Request, content: str = Form(...), db: Session = Depends(get_db)):
-    session_key = request.cookies.get("session_key")
-    todo = create_todo(db, content=content, session_key=session_key)
-    context = {"request": request, "todo": todo}
-    return templates.TemplateResponse("todo/item.html", context)
-
-
-@app.get("/edit/{item_id}", response_class=HTMLResponse)
-def get_edit(request: Request, item_id: int, db: Session = Depends(get_db)):
-    todo = get_todo(db, item_id)
-    context = {"request": request, "todo": todo}
-    return templates.TemplateResponse("todo/form.html", context)
-
-
-@app.put("/edit/{item_id}", response_class=HTMLResponse)
-def put_edit(request: Request, item_id: int, content: str = Form(...), db: Session = Depends(get_db)):
-    todo = update_todo(db, item_id, content)
-    context = {"request": request, "todo": todo}
-    return templates.TemplateResponse("todo/item.html", context)
-
-
-@app.delete("/delete/{item_id}", response_class=Response)
-def delete(item_id: int, db: Session = Depends(get_db)):
-    delete_todo(db, item_id)
-
+    
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("app.app:app", host="0.0.0.0", log_level="info")
